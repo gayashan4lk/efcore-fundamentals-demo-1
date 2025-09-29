d8fdc637e24f17a1e9b7c53c0804c9d71178d1ff
Gayashan Wagachchige BISTEC Global <gayashan@bistecglobal.com>
2025-09-29 21:39:10 +0530

    Add OnConfiguring method to configure SQL Server
    Introduced the `OnConfiguring` method in the `PubContext` class
    within the `PublisherData` namespace. This method configures
    the `DbContext` to use a SQL Server database by calling
    `optionsBuilder.UseSqlServer("Data Source = ");`. The connection
    string is currently incomplete and requires the appropriate
    `Data Source` value to be provided.

--- PublisherData/PubContext.cs
+++ PublisherData/PubContext.cs
@@ -7,5 +7,10 @@ namespace PublisherData
     {
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = ");
+        }
     }
 }
